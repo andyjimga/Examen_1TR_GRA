d5ac7a09cd47ee8fb9bb64c61a26d9dd7ffc172c
Andrés <andyjimga@gmail.com>
2020-11-30 18:48:11 +0100

    todo lo que se hacer
    Deberia haber estudiado más y preguntar mis dudas en clase sin cortarme.

--- Jimenez_Andres/Assets/Scripts/Jugador.cs
+++ Jimenez_Andres/Assets/Scripts/Jugador.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
-    public float Vel = 1000f;
+    public float Vel = 1f;
 
     // Start is called before the first frame update
     void Start()
